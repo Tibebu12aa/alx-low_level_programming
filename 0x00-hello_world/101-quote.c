106edc4f3ca8c67ee65182193da26e84b6f30a00
Tibebu12aa <99277608+Tibebu12aa@users.noreply.github.com>
2022-03-11 04:34:58 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints exactly "and that piece of art is useful"
@@ -8,6 +9,6 @@
  */
 int main(void)
 {
-	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
