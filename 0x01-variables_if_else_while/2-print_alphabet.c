6f119d4fb9eb5890cf7b69841306cd65e4f0e7b5
Tibebu <tibebu12aa@>
2022-03-11 17:35:11 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the alphabert in lowercase,
+ * followed by anew line
+ * Return: Alaways 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch ,= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
